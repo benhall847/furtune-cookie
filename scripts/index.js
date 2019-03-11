d7fe64650325cc2a9fe9053f3550fbdb8b28353a
Benjamin Hallquist <benhall84707@gmail.com>
2019-03-11 15:54:35 -0400

    deleted comment

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,3 @@
-// const theFortunes = ["A person who has nothing looks at all there is and wants something.\nA person who has something looks at all there is and wants all the rest.", "A diplomat is a man who can convince his wife she'd look stout in a fur coat.", "I prefer the most unjust peace to the most righteous war.\n\t\t-- Cicero\n\nEven peace may be purchased at too high a price.\n\t\t-- Poor Richard","\"I take Him shopping with me. I say, 'OK, Jesus, help me find a bargain'\"\n\t\t-- Tammy Faye Bakker", "\"I take Him shopping with me. I say, 'OK, Jesus, help me find a bargain'\"\n\t\t-- Tammy Faye Bakker", "Time is but the stream I go a-fishing in.\n\t\t-- Henry David Thoreau", "\"Kill the Wabbit, Kill the Wabbit, Kill the Wabbit!\"\n\t\t-- Looney Tunes, \"What's Opera Doc?\" (1957, Chuck Jones)"]
-
 
 const myDiv = document.querySelector('[data-div]');
 let index = -1;
@@ -30,7 +28,7 @@ function randomFortune(){
 })
     .then(function (data) { 
     myDiv.textContent = data.fortune;
-    myArray[index] = data.fortune
+    myArray[index] = data.fortune;
 })
     index++
 }
